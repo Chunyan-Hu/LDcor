0a3c306847e2106fb241015190e8161629c82c82
Chunyan-Hu <147682747+Chunyan-Hu@users.noreply.github.com>
2024-04-03 16:29:17 +0800

    Update the_command_list.sh

--- the_command_list.sh
+++ the_command_list.sh
@@ -1,24 +1,18 @@
-#
-#mkdir basic_info/
+
+mkdir basic_info/
 vcftools --gzvcf $2/$1.vcf.gz --remove-filtered-all --hwe 1e-6 --max-missing 1 --counts2 --derived --max-alleles 2 --min-alleles 2--remove-indels --out basic_info/$1 &&  ## --derived must work 
 vcftools --gzvcf $2/$1.vcf.gz --remove-filtered-all --hwe 1e-6 --max-missing 1 --get-INFO AA --max-alleles 2 --min-alleles 2 --remove-indels --out basic_info/$1 &&
-python /media/sheldon/huchy/mosuo/mosuo_latest_2020_10_22/scripts/filteraa_calls.py basic_info/ $1 && # get $1.frq.count.uppercaseaa && $1.INFO.uppercaseaa
+python scripts/filteraa_calls.py basic_info/ $1 && # get $1.frq.count.uppercaseaa && $1.INFO.uppercaseaa
 less basic_info/$1.INFO.uppercaseaa | cut -f1,2 | sed 1d > basic_info/$1.pos.uppercaseaa &&
 vcftools --gzvcf $2/$1.vcf.gz --positions basic_info/$1.pos.uppercaseaa --extract-FORMAT-info GT --remove-indels --out basic_info/$1.uppercaseaa &&
-python /media/sheldon/huchy/mosuo/mosuo_latest_2020_10_22/scripts/ac_find_indivs_data.py basic_info $1 && # get $1.alt.count.indiv
-python /media/sheldon/huchy/mosuo/mosuo_latest_2020_10_22/scripts/write_dots.py basic_info/$1.INFO.uppercaseaa &&
+python scripts/ac_find_indivs_data.py basic_info $1 && # get $1.alt.count.indiv
+python write_dots.py basic_info/$1.INFO.uppercaseaa &&
 mkdir $1 &&
-python /media/sheldon/huchy/mosuo/mosuo_latest_2020_10_22/scripts/mac_find_indivs_data.py basic_info/ $1 uppercaseaa &&
-singularity exec -B /media/sheldon/huchy/program/.vep:/home/.vep,/media/sheldon/huchy/ani_from_zhou/20240227/6mammals/basic_info:/home/basic_info vep.sif vep --cache --offline --species $3 --dir_cache /home/.vep --format vcf -i /home/basic_info/$1.INFO.uppercaseaa.vepinput -o /home/basic_info/$1.vep_output.txt --everything --pick --force_overwrite --fork 60 --cache_version ${5:-101} &&
-## --cache_version 101 by default
-## --cache_version 97 for Mmul_8.0.1
-## --cache_version 90 for gorGor3.1
-## --cache_version 101 for gorGor4
-###sed 's/\t/:/g' basic_info/$1.pos.uppercaseaa >basic_info/$1.pos.uppercaseaa_1 &&
-###awk 'NR==FNR{a[$1];next}$2 in a' basic_info/$1.pos.uppercaseaa_1 vcf_files/$3.vep_output.txt >$1/$1.vep_output.txt &&
+python scripts/mac_find_indivs_data.py basic_info/ $1 uppercaseaa &&
+singularity exec -B /path/to/local/.vep:/home/.vep,/path/to/this/basic_info:/home/basic_info vep.sif vep --cache --offline --species $3 --dir_cache /home/.vep --format vcf -i /home/basic_info/$1.INFO.uppercaseaa.vepinput -o /home/basic_info/$1.vep_output.txt --everything --pick --force_overwrite --fork 8 --cache_version ${5:-101} &&
 grep -v "#" basic_info/$1.vep_output.txt | cut -f 2,4,7 > $1/$1.functype.vep.txt &&
 cp basic_info/$1.frq.count.indiv.uppercaseaa $1/ &&
-python /media/sheldon/huchy/mosuo/mosuo_latest_2020_10_22/scripts/merge_variants_types_given_vep.py $1/ $1 vep &&
+python scripts/merge_variants_types_given_vep.py $1/ $1 vep &&
 gzip basic_info/$1.uppercaseaa.GT.FORMAT &&
 cd $1/ &&
 { 
@@ -36,9 +30,7 @@ cat $1.frq.count.indiv.splice.vep.uppercaseaa $1.frq.count.indiv.nonsense.vep.up
 cd ../ &&
 for ii in {"splicenonsense","missense","codingsynononly"}
 do
-	#python /media/sheldon/huchy/ani_from_zhou/slim/2.mac_count_all_data_byloci_chr.py $4 $1/ $1 vep $ii $1/
-	python /media/sheldon/huchy/ani_from_zhou/slim/2.mac_count_all_data_byloci_chr.py samples/$4.sample.txt $1/ $1 vep $ii $1/ 
-	#gzip $1/$1.frq.count.indiv.$ii.vep.uppercaseaa 
+	python /media/sheldon/huchy/ani_from_zhou/slim/2.mac_count_all_data_byloci_chr.py basic_info/$4.sample.txt $1/ $1 vep $ii $1/ 
 done
 echo done
 
